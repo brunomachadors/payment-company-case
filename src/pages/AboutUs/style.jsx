b083510a97e721d427f14f1225e87eae16817680
brunomachadors <brunomachadors@gmail.com>
2023-12-14 23:10:38 +0000

    About us

---
+++ src/pages/AboutUs/style.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const AboutUsView = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  gap: 3vh;
+`;
+
+export const PageTitle = styled.p`
+  font-size: 3vh;
+  color: black;
+  text-align: center;
+`;
